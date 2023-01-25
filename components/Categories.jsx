8a230db7990baf4554c5395433b51f4c5363cbde
Jai Senthil Kumar <dhananjai284@gmail.com>
2023-01-25 11:37:33 -0500

    Added Categories to home page

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -6,22 +6,23 @@ const Categories = () => {
   const [categories, setCategories] = useState([])
 
   useEffect(() => {
-    getCategories()
-      .then((result) => setCategories(result))
-  })
+    getCategories().then((result) => {
+        setCategories(result)
+      });
+  }, [])
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 mb-8 pb-12">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-4 font-semibold border-b pb-4">
         Categories
       </h3>
-      {categories.map((category) => {
-        <Link key={category.slug} href={`/category/${category.slug}`}>
-          <span className="hover:text-orange-400 cursor-pointer block pb-3 mb-3">
+      {categories.map((category, index) => (
+        <Link key={index} href={`/category/${category.slug}`}>
+          <span className="hover:text-orange-500 cursor-pointer block pb-3 mb-3">
             {category.name}
           </span>
         </Link>
-      })}
+      ))}
     </div>
   )
 }
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -20,7 +20,7 @@ const PostWidget = ({categories, slug}) => {
   
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
-      <h3 className="text-xl mb-4 font-semibold border-b">
+      <h3 className="text-xl mb-4 font-semibold border-b pb-4">
         {slug ? 'Related Posts' : "Recent Posts"}
       </h3>
       {relatedPosts.map((post) => (
